f1dffbbb74fc7bfb8933bef4adb3f27141db475b
dandytech <dannkwo@gmail.com>
2023-11-27 10:04:08 +0100

    my second commit

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,8 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
-import UpdateUserDataForm from "../features/authentication/updateUserDataForm";
+import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
+
 function Account() {
   return (
     <>
